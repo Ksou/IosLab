c4fa22dcdb83524c4d06cf5647d2aa042c972d6d
Keith Wilson <dreamihad@gmail.com>
2013-01-07 21:50:34 -0500

    Big update

--- Resources/CoreFiles/aPro.js
+++ Resources/CoreFiles/aPro.js
@@ -10,12 +10,14 @@ function aPro(table, data) {
     table.appendRow(RowNew);
     table.addEventListener("click", function(e) {
         if (e.index == table.data[0].rows.length - 1) Back(); else {
-            var API = "JM", tempString = data[e.index].title, Sounder = "http://storage-new.newjamendo.com/download/track/" + data[e.index].id + "/mp32";
+            var Sounder, API = "JM", tempString = data[e.index].id;
+            Sounder = "http://storage-new.newjamendo.com/download/track/" + tempString + "/mp32";
             if (data[e.index].name == null) {
                 API = " SC";
-                Sounder = data[e.index].permalink_url + "/" + "/download.mp3";
+                Sounder = data[e.index].permalink_url + "/download.mp3";
             }
-            alert("Playing " + Sounder + API);
+            alert("Playing " + Sounder);
+            (Ti.Platform.osname == "android" || Ti.Platform.osname == "iphone" || Ti.Platform.osname == "ipad") && Songer(Sounder);
         }
     });
 }
@@ -26,9 +28,11 @@ function Songer(Song) {
     });
     RowPlay.play();
     Alloy.Globals.Player = RowPlay;
+    (Ti.Platform.osname == "iphone" || Ti.Platform.osname == "ipad") && Alloy.Globals.MainWin.setRightNavButton(Alloy.Globals.StopButton);
 }
 
 function Back() {
+    (Ti.Platform.osname == "iphone" || Ti.Platform.osname == "ipad") && Alloy.Globals.MainWin.setLeftNavButton(Alloy.Globals.InfoButton);
     Alloy.Globals.Alerts && alert("Fall Back");
     for (var i = 0; i < Alloy.Globals.UIStuff.length; i++) i != 3 ? Alloy.Globals.UIStuff[i].visible = !0 : Alloy.Globals.UIStuff[i].visible = !1;
 }
